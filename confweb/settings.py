1e64b84b4d59f7181b635d57dd57dc18b9d7143e
Keith Yang <yang@keitheis.org>
2013-09-30 16:10:05 +0800

    Minor update of mezzanine settings and required version of 1.4.15

--- confweb/settings.py
+++ confweb/settings.py
@@ -98,14 +98,14 @@ MANAGERS = ADMINS
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+TIME_ZONE = "Asia/Taipei"
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "en"
+LANGUAGE_CODE = "zh-TW"
 
 # A boolean that turns on/off debug mode. When set to ``True``, stack traces
 # are displayed for error pages. Should always be set to ``False`` in
