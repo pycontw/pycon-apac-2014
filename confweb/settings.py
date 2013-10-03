f98e273b137712179e75f6ebd6c6d72d501b176b
Jim Yeh <lemonlatte@gmail.com>
2013-10-03 14:03:30 +0800

    Add ALLOWED_HOSTS in settings.py
    * It is required by django1.5

--- confweb/settings.py
+++ confweb/settings.py
@@ -91,6 +91,12 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
+ALLOWED_HOSTS = [
+    "tw.pycon.org",
+    "localhost",
+    "127.0.0.1"
+]
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
