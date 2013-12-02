70c6c06fb298fe2798db76e066ecac2c3b900d46
Art Pai <minipai@gmail.com>
2013-12-02 12:13:17 +0800

    set demo route and template

--- confweb/urls.py
+++ confweb/urls.py
@@ -22,6 +22,7 @@ urlpatterns = patterns("",
     # Using confweb.views.i18n isntead
     #(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^setlang/$', 'confweb.views.i18n.set_language', name='set_language'),
+    url(r'^demo/(?P<page>\w+)/$', 'confweb.views.demo.show', name='demo'),
 
     # We don't want to presume how your homepage works, so here are a
     # few patterns you can use to set it up.
---
+++ confweb/views/demo.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def show(request, page='show'):
+
+    data = {'foo': 'bar'}
+
+    if request.is_ajax():
+        template = 'demo/' + page + '_content.html'
+    else:
+        template = 'demo/' + page + '.html'
+
+    return render(request, template, data)
+
