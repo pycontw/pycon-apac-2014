03eb23f01c2c068f1280ea42bb03f081f6f38297
Jim Yeh <lemonlatte@gmail.com>
2013-12-14 01:03:54 +0800

    Add basic views for proposal listing and proposal creation.

--- proposal/views.py
+++ proposal/views.py
@@ -1,21 +1,45 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
+from .forms import ProposalForm
+from .models import ProposalModel
 
 
-def create_proposal(self):
+@login_required
+def create_proposal(request):
+
+    if request.method == "POST":
+
+        proposal_form = ProposalForm(request.POST)
+
+        if proposal_form.is_valid():
+            return redirect(reverse("proposal:list"))
+        else:
+            return render(request, "proposal/create.html",
+                      {"proposal_form": proposal_form})
+    else:
+
+        proposal_form = ProposalForm()
+
+        return render(request, "proposal/create.html",
+                      {"proposal_form": proposal_form})
+
+
+@login_required
+def list_proposal(request):
+
+    my_proposal = ProposalModel.objects.filter(author=request.user)
+    return render(request, "proposal/list.html", {"my_proposal": my_proposal})
+
+
+@login_required
+def update_proposal(request):
 
     return HttpResponse("")
 
 
-def list_proposal(self):
-
-    return HttpResponse("")
-
-
-def update_proposal(self):
-
-    return HttpResponse("")
-
-
-def delete_proposal(self):
+@login_required
+def delete_proposal(request):
 
     return HttpResponse("")
