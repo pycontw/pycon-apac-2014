8b3b78cf07e839f81e3231cec89a26dbc7c41af0
c3h3 <c3h3.tw@gmail.com>
2013-12-20 11:09:59 +0800

    Add ProposalModelAdmin on the backend admin panel and filtering viewer by its account

--- proposal/admin.py
+++ proposal/admin.py
@@ -8,6 +8,7 @@ Created on Dec 20, 2013
 from django.contrib import admin
 from models import ProposalModel
 from forms import ProposalForm
+from django.contrib.auth.models import User
 
 class ProposalModelAdmin(admin.ModelAdmin):
     list_display = ("id","create_on","last_modified",'title', 'author', 'speech_type',
@@ -19,8 +20,11 @@ class ProposalModelAdmin(admin.ModelAdmin):
     
     def get_queryset(self, request):
         qs = super(ProposalModelAdmin, self).get_queryset(request)
-        if request.user.is_superuser:
+        
+        if request.user in User.objects.filter(username = u"program"):
             return qs
-        return qs.filter(author=request.user)
+        
+        else:
+            return qs.filter(author = request.user)
     
 admin.site.register(ProposalModel, ProposalModelAdmin)
