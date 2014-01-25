be55e8aceae07ef3b268a83dcb2d8f49ccab1fc2
Keith Yang <yang@keitheis.org>
2014-01-25 11:21:21 +0800

    More on translation

---
+++ conweb/locale/missing.py
@@ -0,0 +1,12 @@
+"""
+Add missing translation string here
+"""
+from django.utils.translation import ugettext_lazy as _
+
+if 0:  # Let Python discard this block
+    _("Username or email address")
+    _("username")
+    _("email address")
+    _("Password")
+    _("Password (again)")
+    _("Only letters, numbers, dashes or underscores please")
