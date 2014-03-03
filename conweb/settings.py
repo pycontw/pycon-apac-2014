78a9184ca5222463853c80e3b1b33184fc52a279
Keith Yang <yang@keitheis.org>
2014-03-03 09:57:56 +0800

    Allow more HTML5 tags in RichText page content

--- conweb/settings.py
+++ conweb/settings.py
@@ -314,6 +314,21 @@ MIDDLEWARE_CLASSES = (
 PACKAGE_NAME_FILEBROWSER = "filebrowser_safe"
 PACKAGE_NAME_GRAPPELLI = "grappelli_safe"
 
+RICHTEXT_ALLOWED_TAGS = (
+    u'video', u'header', u'article', u'output', u'details', u'figure',
+    u'figcation', u'progress', u'meter', u'datalist',
+    u'main', u'article', u'aside', u'footer', u'header', u'nav',
+    u'a', u'abbr', u'acronym', u'address', u'area', u'b', u'bdo', u'big',
+    u'blockquote', u'br', u'button', u'caption', u'center', u'cite', u'code',
+    u'col', u'colgroup', u'dd', u'del', u'dfn', u'dir', u'div', u'dl', u'dt',
+    u'em', u'fieldset', u'font', u'form', u'h1', u'h2', u'h3', u'h4', u'h5',
+    u'h6', u'hr', u'i', u'img', u'input', u'ins', u'kbd', u'label', u'legend',
+    u'li', u'map', u'menu', u'ol', u'optgroup', u'option', u'p', u'pre', u'q',
+    u's', u'samp', u'select', u'small', u'span', u'strike', u'strong', u'sub',
+    u'sup', u'table', u'tbody', u'td', u'textarea', u'tfoot', u'th', u'thead',
+    u'tr', u'tt', u'u', u'ul', u'var', u'wbr'
+)
+
 #########################
 # OPTIONAL APPLICATIONS #
 #########################
