19129859edfaa4b0db93a68cd069f0e0d1a784fd
Keith Yang <yang@keitheis.org>
2014-03-31 14:58:17 +0800

    Tweak speakers page layout

--- speaker/views.py
+++ speaker/views.py
@@ -6,18 +6,29 @@ from django.http import Http404
 from proposal.models import ProposalModel
 
 
-def list_speakers(request):
-    author_of_accept_proposals = ProposalModel.objects.filter(result__decision=1) \
+def _get_author_of_accept_proposals():
+    author_of_accept_proposals = ProposalModel.objects \
+        .filter(result__decision=1) \
         .values_list("author", flat=True)
-    speakers = User.objects.filter(id__in=author_of_accept_proposals)
+    return author_of_accept_proposals
 
+
+def list_speakers(request):
+    author_of_accept_proposals = _get_author_of_accept_proposals()
+    speakers = User.objects.filter(id__in=author_of_accept_proposals)
     return render(request, "list_speakers.html", {"speakers": speakers})
 
 
 def speaker_info(request, speaker_id):
-    author_of_accept_proposals = ProposalModel.objects.filter(result__decision=1) \
-        .values_list("author", flat=True)
+    author_of_accept_proposals = _get_author_of_accept_proposals()
     if not int(speaker_id) in author_of_accept_proposals:
         raise Http404
+
     speaker = User.objects.get(id=speaker_id)
-    return render(request, "speaker_info.html", {"speaker": speaker})
+    profile = speaker.get_profile()
+    picture = profile.picture
+    og_image_url = picture.url if picture else None
+    return render(request, "speaker_info.html",
+                  {"speaker": speaker,
+                   "profile": profile,
+                   "og_image_url": og_image_url})
